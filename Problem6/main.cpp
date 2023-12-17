ea0a153cbca13ed61807f7bceb1d76208379dc0f
Khaled Salah <k.salah0610@gmail.com>
2023-12-17 19:27:16 +0200

    Solve problem 6

---
+++ Problem6/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string firstname, lastname, fullname;
+    cout << "Enter your first name:" << endl;
+    cin >> firstname;
+    cout << "Enter your second name:" << endl;
+    cin >> lastname;
+    cout << firstname << " " << lastname;
+    return 0;
+}
